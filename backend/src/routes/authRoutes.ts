57a522d687604aa704a783955090cd248f5cd5ab
Kaj Borek <zgierek98@gmail.com>
2026-04-13 01:02:38 +0200

    trying to work on unwanted error

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -30,7 +30,7 @@ router.post('/register', async (req: Request, res: Response) => {
     res.status(201).json({ message: 'User registered successfully!' });
   } catch (err: any) {
     console.error(err);
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: 'nie pinga nic' });
   }
 });
 
